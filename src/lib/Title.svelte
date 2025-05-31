564d69d73895f7e8f085dea360e3c6bef07497fd
Richard Haugen Olsen <richard.haugen.olsen@miles.no>
2025-05-31 11:32:36 +0200

    ..

---
+++ src/lib/Title.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+
+let {title = "hei"} = $props()
+
+let fullTitle = $derived(title + "" + new Date().toLocaleDateString());
+
+
+</script>
+
+
+<h1>{fullTitle}</h1>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
+<script>
+	import Title from '$lib/Title.svelte';
+</script>
+
 <h1>Speed consulting</h1>
+<Title title="Hei hei" />
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
